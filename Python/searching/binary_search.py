79f2ea1e74fed60205f874d62daf0d0992d6a198
fidelisaboke <fidelisaboke@gmail.com>
2023-07-04 11:18:21 +0300

    Modified binary_search

--- Python/searching/binary_search.py
+++ Python/searching/binary_search.py
@@ -22,29 +22,9 @@ Start
     until low > high
 Stop
 '''
-# Problem data:
-numbers = [1, 3, 5, 17, 38, 43, 71]
-target = 38
 
-
-# Solution to the problem:
-
-# Creating the binary_search function in python
-def binary_search(numbers , target):
-    low = 0
-    high = len(numbers)-1    
-
-    while (high >= low):
-        mid = (high+low)//2
-        if numbers[mid] == target:
-            return mid
-        elif numbers[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-
-# Recursive version of the above function
-def rec_binary_search(numbers, target, low, high):
+# Recursive binary search
+def binary_search(numbers, target, low, high):
     if low > high:
         return False
     else:
@@ -52,13 +32,66 @@ def rec_binary_search(numbers, target, low, high):
         if numbers[mid] == target:
             return mid
         elif numbers[mid] < target:
-            return rec_binary_search(numbers, target, mid+1, high)
+            return binary_search(numbers, target, mid+1, high)
         else:
-            return rec_binary_search(numbers, target, low, mid-1)
+            return binary_search(numbers, target, low, mid-1)
+        
+        
+# Slightly different interpretation but works the same way anyway
+def alt_binary_search(numbers, target, low, high):
+    if low > high:
+        return False
+    else:
+        mid = low + (high-low)//2 # is this the same as (high+low)//2
+        if numbers[mid] == target:
+            return mid
+        elif numbers[mid] < target:
+            return binary_search(numbers, target, mid+1, high)
+        else:
+            return binary_search(numbers, target, low, mid-1)
+        
+        
+# Running the binary search program:
+from time import sleep
 
+numbers = []
+target = 0
 
-# Calling the function with the given data
-result = binary_search(numbers, target)
-result_rec = rec_binary_search(numbers, target, 0, len(numbers)-1)
-print(f'The number {target} is in index: {result}') # Prints '4'
-print(f'(Recursive function) The number {target} is in index: {result}')
+while True:
+    print(f'''
+          Binary search programme
+          Select Option:
+          1. Insert Data
+          2. Find Element
+          0. Exit
+          
+          Current List: {numbers}
+          ''')
+    
+    option = input('>')
+    if option == '1':
+        try:
+            number = int(input('Enter data element:'))
+            numbers.append(number)
+            sleep(1)
+        except ValueError:
+            print('Please enter an integer.')
+            
+    elif option == '2':
+        try:
+            target = int(input('Enter the number to search:'))
+            sorted_nums = sorted(numbers)
+            sleep(1)
+            index = binary_search(sorted_nums, target, 0, len(numbers)-1)
+            index_alt = alt_binary_search(sorted_nums, target, 0, len(numbers)-1)
+            if(index or index_alt == False):
+                print('Number not in the list')
+            else:
+                print(f'The number {target} is in index: {index}')
+                print(f'The number {target} using the alt binary search is in index: {index_alt}')
+        except ValueError:
+            print('Please enter an integer.')
+    elif option == '0':
+        print('Exiting script...')
+        sleep(1)
+        break
